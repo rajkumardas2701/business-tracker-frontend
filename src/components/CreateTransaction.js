226a76b0f4a3b0efcd53e5a479512d7a0c231ebd
Rajkumar Das <rajkumardas2701@gmail.com>
2023-02-16 23:17:08 +0530

    add create Transaction form

--- src/components/CreateTransaction.js
+++ src/components/CreateTransaction.js
@@ -1,3 +1,108 @@
-const CreateTransaction = () => {
-  
-}
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import '../styles/AuthForm.css';
+
+const CreateTransaction = ({ setShowCreateTransaction }) => {
+  const [date, setDate] = useState('');
+  const [actionBy, setActionBy] = useState('');
+  const [sentReceive, setSentReceive] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [remark, setRemark] = useState('');
+
+  const handleFormCancel = (e) => {
+    setDate('');
+    setActionBy('');
+    setSentReceive('');
+    setAmount(0);
+    setRemark(0.0);
+    setShowCreateTransaction(false);
+    e.preventDefault();
+  };
+
+  const handleSubmit = () => {
+
+  };
+
+  return (
+    <div className="form-container create-deal-container">
+      <h2>Enter a new Transaction</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-elements">
+          <p>
+            Date:
+          </p>
+          <input
+            placeholder="Transaction Date"
+            type="date"
+            name="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-elements">
+          <p>
+            Amount:
+          </p>
+          <input
+            placeholder="Enter Amount"
+            type="number"
+            name="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-elements">
+          <p>
+            Sent/Received:
+          </p>
+          <input
+            placeholder="Amount Sent or Received"
+            type="text"
+            name="sentReceive"
+            value={sentReceive}
+            onChange={(e) => setSentReceive(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-elements">
+          <p>
+            Transacted By:
+          </p>
+          <input
+            placeholder="Who performed transaction"
+            type="text"
+            name="actionBy"
+            value={actionBy}
+            onChange={(e) => setActionBy(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-elements">
+          <p>
+            Remark:
+          </p>
+          <input
+            placeholder="like GPay'd someone"
+            type="text"
+            name="remark"
+            value={remark}
+            onChange={(e) => setRemark(e.target.value)}
+            required
+          />
+        </div>
+        <div className="btn-container">
+          <button type="submit" className="form-btn">Submit</button>
+          <button type="submit" className="form-btn" onClick={handleFormCancel}>Cancel</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+CreateTransaction.propTypes = {
+  setShowCreateTransaction: PropTypes.func.isRequired,
+};
+
+export default CreateTransaction;
--- src/containers/Deals.js
+++ src/containers/Deals.js
@@ -2,14 +2,20 @@ import { useContext, useState } from 'react';
 import DashboardContext from '../contexts/DashboardContext';
 import Deal from '../components/Deal';
 import CreateDeal from '../components/CreateDeal';
-import '../styles/Deals.css';
 import DealTransactions from './DealTransactions';
+import CreateTransaction from '../components/CreateTransaction';
+import '../styles/Deals.css';
 
 const Deals = () => {
   const { deals } = useContext(DashboardContext);
   const [showCreateDeal, setShowCreateDeal] = useState(false);
+  const [showCreateTransaction, setShowCreateTransaction] = useState(false);
   const handleCreateDeal = (e) => {
     setShowCreateDeal(!showCreateDeal);
+    e.preventDefault();
+  };
+  const handleCreateTransaction = (e) => {
+    setShowCreateTransaction(!showCreateTransaction);
     e.preventDefault();
   };
   return (
@@ -31,7 +37,13 @@ const Deals = () => {
       </div>
 
       <button type="submit" onClick={handleCreateDeal} className="create-deal">Create Deal</button>
+      <button type="submit" onClick={handleCreateTransaction} className="create-transaction">Enter Transaction</button>
       {showCreateDeal && <CreateDeal setShowCreateDeal={setShowCreateDeal} />}
+      {showCreateTransaction && (
+      <CreateTransaction
+        setShowCreateTransaction={setShowCreateTransaction}
+      />
+      )}
     </div>
   );
 };
