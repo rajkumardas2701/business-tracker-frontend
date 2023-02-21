26f851231ac00b6fe55bbc38cf3dc02d47c3ef7f
Rajkumar Das <rajkumardas2701@gmail.com>
2023-02-21 12:37:00 +0530

    add delete option on deal related transaction

--- src/containers/DealTransactions.js
+++ src/containers/DealTransactions.js
@@ -1,58 +1,70 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import Transaction from '../components/Transaction';
+import DeleteWarning from '../layouts/DeleteWarning';
 
-const DealTransactions = ({ dealTransacts, dealName }) => (
-  <div className="deals-transactions-container">
-    <div className="side-transactions-list">
-      <h4 style={{ marginTop: '10px', color: 'blue' }}>{(dealName === 'No Deal selected') ? 'Please select one of the deal to see Transactions' : `Vehicle: ${dealName.slice(0, -6).toUpperCase()} & Date: ${dealName.slice(-8, -4)}-${dealName.slice(-4, -2)}-${dealName.slice(-2)}`}</h4>
-      <table>
-        <thead style={{ textAlign: 'center', margin: 'auto' }}>
-          <tr>
-            <td style={{ width: '15%' }}>
-              Date
-            </td>
-            <td>
-              Amount
-            </td>
-            <td>
-              Sent/Received
-            </td>
-            <td style={{ width: '15%' }}>
-              Action by
-            </td>
-            <td style={{ width: '30%' }}>
-              Remark
-            </td>
-            <td>
-              Edit
-            </td>
-            <td>
-              Delete
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          {(dealTransacts && dealTransacts.length)
-            ? dealTransacts.map((tx) => (
-              <Transaction
-                key={tx.id}
-                sTx={tx}
-              />
-            ))
-            : (
-              <tr>
-                <td colSpan="7">
-                  There&apos;s no transactions under this deal
-                </td>
-              </tr>
-            )}
-        </tbody>
-      </table>
-      {/* {showDeleteWarning &&
-        <DeleteWarning fn={setShowDeleteWarning} deleteTxID={deleteTxID} />} */}
+const DealTransactions = ({ dealTransacts, dealName }) => {
+  const [showDeleteWarning, setShowDeleteWarning] = useState(false);
+  const [deleteTxID, setDeleteTxID] = useState(0);
+  return (
+    <div className="deals-transactions-container">
+      <div className="side-transactions-list">
+        <h4 style={{ marginTop: '10px', color: 'blue' }}>
+          {(dealName === 'No Deal selected') ? 'Please select one of the deal to see Transactions'
+            : `Vehicle: ${dealName.slice(0, -6).toUpperCase()} & Date: ${dealName.slice(-8, -4)}-${dealName.slice(-4, -2)}-${dealName.slice(-2)}`}
+        </h4>
+        <table>
+          <thead style={{ textAlign: 'center', margin: 'auto' }}>
+            <tr>
+              <td style={{ width: '15%' }}>
+                Date
+              </td>
+              <td>
+                Amount
+              </td>
+              <td>
+                Sent/Received
+              </td>
+              <td style={{ width: '15%' }}>
+                Action by
+              </td>
+              <td style={{ width: '30%' }}>
+                Remark
+              </td>
+              <td>
+                Edit
+              </td>
+              <td>
+                Delete
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {(dealTransacts && dealTransacts.length)
+              ? dealTransacts.map((tx) => (
+                <Transaction
+                  key={tx.id}
+                  sTx={tx}
+                  setShowDeleteWarning={setShowDeleteWarning}
+                  showDeleteWarning={showDeleteWarning}
+                  setDeleteTxID={setDeleteTxID}
+                />
+              ))
+              : (
+                <tr>
+                  <td colSpan="7">
+                    There&apos;s no transactions under this deal
+                  </td>
+                </tr>
+              )}
+          </tbody>
+        </table>
+        {showDeleteWarning
+          && <DeleteWarning fn={setShowDeleteWarning} deleteTxID={deleteTxID} />}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 DealTransactions.propTypes = {
   dealTransacts: PropTypes.oneOfType([PropTypes.array]).isRequired,
