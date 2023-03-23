9a8371c71e00a53bbb0c10dbe0f29dff98c1cd33
Rajkumar Das <rajkumardas2701@gmail.com>
2023-03-23 18:17:55 +0530

    update api url

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,4 @@
-// const apiURL = 'https://businesstrackerbackendapi.azurewebsites.net';
-const apiURL = 'http://localhost:3000';
+const apiURL = 'https://businesstrackerbackendapi.azurewebsites.net';
+// const apiURL = 'http://localhost:3000';
 
 export default apiURL;
