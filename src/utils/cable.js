923bcf4c857987e782715672d8324a764ca3b754
Rajkumar Das <rajkumardas2701@gmail.com>
2023-03-23 18:24:03 +0530

    update websocket url

--- src/utils/cable.js
+++ src/utils/cable.js
@@ -1,6 +1,7 @@
 import { createConsumer } from '@rails/actioncable';
 
-const URL = 'ws://localhost:3000/cable';
+// const URL = 'ws://localhost:3000/cable';
+const URL = 'ws://businesstrackerbackendapi.azurewebsites.net/cable';
 const consumer = createConsumer(URL);
 
 export default consumer;
