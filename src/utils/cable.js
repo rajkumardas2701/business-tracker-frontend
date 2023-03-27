9d425b2508cb32c3ec0b10188e65bfafc7451231
Rajkumar Das <rajkumardas2701@gmail.com>
2023-03-27 18:49:39 +0530

    Update cable.js

--- src/utils/cable.js
+++ src/utils/cable.js
@@ -1,7 +1,7 @@
 import { createConsumer } from '@rails/actioncable';
 
 // const URL = 'ws://localhost:3000/cable';
-const URL = 'ws://businesstrackerbackendapi.azurewebsites.net/cable';
+const URL = 'wss://businesstrackerbackendapi.azurewebsites.net/cable';
 const consumer = createConsumer(URL);
 
 export default consumer;
